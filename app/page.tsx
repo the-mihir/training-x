39d295db39c1b371a595259ad01e0959c7066e31
Mihir <mihirkantho@gmail.com>
2025-05-19 14:34:24 +0600

    Fixing SSRF Error for client site render

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,6 @@
 import { Suspense } from "react"
-import dynamic from "next/dynamic"
 import { Skeleton } from "@/components/ui/skeleton"
 import ClientWrapper from "./client-wrapper"
-
-// Dynamically import the home client component with SSR disabled
-// This ensures it only runs on the client side
-const DynamicHomeClient = dynamic(() => import("./home-client"), {
-  loading: () => (
-    <div className="min-h-screen flex items-center justify-center">
-      <Skeleton className="h-[80vh] w-full max-w-6xl mx-auto rounded-xl" />
-    </div>
-  ),
-  ssr: false, // Disable SSR completely for this component
-})
 
 export default function Home() {
   return (
